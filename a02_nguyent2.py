b0ba25cb9a44fec5985e3ae48081687356527a60
nguyent2 <nguyent2@berea.edu>
2019-09-11 06:59:07 -0400

    Add description to all functions.

--- a02_nguyent2.py
+++ a02_nguyent2.py
@@ -12,7 +12,7 @@
 # https://www.w3schools.com/colors/colors_picker.asp
 # Getting color background from https://www.w3schools.com/colors/colors_picker.asp
 # https://docs.python.org/3.3/library/turtle.html?highlight=turtle#
-
+# http://cs.berea.edu/courses/csc226book/
 ######################################################################
 
 import turtle
@@ -20,25 +20,49 @@ from math import pi
 import random
 
 def main():
+    """
+    This is the main function. The main() function is used as a "container" where all of the other functions are called to be executed.
+    """
 
     wns = turtle.Screen()
 
     def banh_xe(bichthuy):
+        """
+        This function just sets up the turtle ready to draw a lot of circles.
+        :param bichthuy:
+        :return:none
+
+        :param bichthuy:
+        :return:none
+        """/
+
         bichthuy.shape("circle")
         bichthuy.shapesize(0.1,0.1)
-        bichthuy.speed(10)
+        bichthuy.speed(0)
         bichthuy.pensize(6)
 
         def ve_hinh_tron(number,cao):
+            """
+            This function draws a full circle (which serves as a wheel in a car)
+            :param number:
+            :param cao:
+            :return: none
+            """
             for i in range(number):
                 bichthuy.right(1)
                 bichthuy.forward(cao)
 
         def ve_tron(solieu, chieucao):
+            """
+            This function draws backward of a circle, to connect to the next step
+            :param solieu:
+            :param chieucao:
+            :return: none
+            """
             for i in range(solieu):
                 bichthuy.left(1)
                 bichthuy.backward(chieucao)
-
+        # Call the function to draw wheels
         ve_hinh_tron(360, 0.5)
         ve_tron(90, 0.5)
         bichthuy.left(90)
@@ -56,6 +80,12 @@ def main():
     wns.bgcolor("#e6ffff")
 
     def annyong(mauu, yuri):
+        """
+        This function draws a whole car and fill it
+        :param mauu:
+        :param yuri:
+        :return: none
+        """
         ngoc = turtle.Turtle()
         ngoc.color("#000080")
         ngoc.fillcolor(yuri)
@@ -85,6 +115,11 @@ def main():
     annyong(2*150, lan_nay_8)
 
     def nut_that(brother):
+        """
+        This function draws a two point, of a line to ask for calculating the length
+        :param brother:
+        :return:
+        """
         abcxyz = turtle.Turtle()
         abcxyz.color("#ffff00")
         abcxyz.shape("circle")
@@ -114,6 +149,12 @@ def main():
     conrua.forward(((900/pi)-(80+180/pi))*8+180/pi)
 
     def dau_cuoi_tuong_ung(chieu_rong, chu_cai):
+        """
+        This function appears the point A and B
+        :param chieu_rong:
+        :param chu_cai:
+        :return:
+        """
         thy = turtle.Turtle()
         thy.color("#660066")
         thy.pensize(6)
@@ -127,10 +168,17 @@ def main():
     dau_cuoi_tuong_ung(-4*150-45,"A")
     dau_cuoi_tuong_ung(4*150+50, "B")
 
+    #This ask for the user's input
     answer = wns.numinput("Can you calculate the length of AB ?", "Your answer:  ", default=None, minval=0,
                           maxval=10000000)
 
     def written(soluong, vietchu):
+        """
+        This function asks questions, and appears user's answer
+        :param soluong:
+        :param vietchu:
+        :return: none
+        """
         jordan = turtle.Turtle()
         jordan.color("#660066")
         jordan.pensize(6)
